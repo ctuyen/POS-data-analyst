c747c9e5d15a81f3715aafc6ea9760560d1a6772
NhaTraGop <nhatragopbinhduong@gmail.com>
2019-12-21 00:52:10 +0700

    Complete template 1

--- index.js
+++ index.js
@@ -6,62 +6,82 @@ let reportThree = [];
 let reportFour = [];
 
 const workSheets = xlsx.parse(`${__dirname}/MM-POS-Data-Test-Report-1.xlsx`);
-// .data[1][0] is row 1 col 0
-console.log('info...', workSheets[0].data[1][4], workSheets[0].data[4700][4]);
-updateReportOne();
+reportOne = updateReportOne();
 console.log('reportOne...', reportOne);
 
+
+// REPORT ONE
+
 function updateReportOne() {
-    let storecodeList = [];
-    let barcodeList = [];
-    let totalQuantity = 0;
-    let dateActive;
+
+    let dateOrderSet = new Set()
+    let productMap = new Map()
+    let lastDateOrder
 
     for (const [i, row] of workSheets[0].data.entries()) {
-        if (i > 10) break; //testing
         if (i === 0) continue; // header
-
+        
         let storecode = row[0].split(':')[0];
         let storename = row[0].split(':')[1];
         let productname = row[1].split('] ')[1];
         let barcode = row[2];
-        let UoM = row[3];
+        let uoM = row[3];
         let dateOrder = row[4];
-        let quantity = Number(row[5]);
+        let quantity = Number(row[6]);
+        let keyString = storename + '🖕' + storecode + '🖕' + barcode + '🖕' + productname + '🖕' + uoM;
 
-        // initial
-        if (i === 1) {
-            reportOne = [[storecode, storename]];
-            storecodeList.push(storecode);
-            barcodeList.push(barcode);
-            dateActive = dateOrder;
-            continue;
+        if (i === 1){
+          lastDateOrder = dateOrder;
+        }
+        
+        if (productMap.has(keyString)){
+          if (dateOrderSet.has(dateOrder+barcode)) {
+            let currentQuantityArray = productMap.get(keyString);
+            currentQuantityArray.pop();
+            let currentQuantity = productMap.get(keyString)[productMap.get(keyString).length-1];
+            productMap.set(keyString, [...currentQuantityArray, currentQuantity + quantity]);
+          }
+          else{
+            let currentQuantityArray = productMap.get(keyString);
+            let zerosArray = [];
+            let dateGap = dateOrder - lastDateOrder;
+            for (let i = 0; i < dateGap; i++) {
+              zerosArray.push(0);
+            }
+            productMap.set(keyString, [...currentQuantityArray, ...zerosArray, quantity]);
+            dateOrderSet.add(dateOrder+barcode);
+          }
+        } else{
+          productMap.set(keyString,[quantity]);
         }
 
-        if (!storecodeList.includes(storecode)) {
-            storecodeList.push(storecode);
-            barcodeList.push(barcode);
-            reportOne = [...reportOne, [storecode, storename, productname, barcode, UoM]];
-            totalQuantity = quantity;
-            dateActive = dateOrder;
-        }
-        else if (!barcodeList.includes(barcode)) {
-            barcodeList.push(barcode);
-            reportOne = [...reportOne, [storecode, storename, productname, barcode, UoM]];
-            totalQuantity = quantity;
-            dateActive = dateOrder;
-        }
-        else if (dateOrder !== dateActive) {
-            // reportOne[reportOne.length-1].push(totalQuantity);
-            let index = reportOne.findIndex(row => {
-                return row[3] === barcode;
-            });
-            reportOne[index].push(totalQuantity);
-            totalQuantity = quantity;
-        }
-        else {
-            totalQuantity += quantity;
-        }
-        // console.log('dateactive', dateActive);
+        lastDateOrder = dateOrder;
     }
+
+    let resultArray = [];
+
+    productMap.forEach((value, key, map) => {
+      // key: storecode + '-' + barcode
+      // value: [... cac quantity cua cac ngay]
+      // storename - productname - barcode - uoM - ... quantity cac ngay
+      let infos = key.split('🖕');
+      let storename = infos[0];
+      let storecode = infos[1];
+      let barcode = infos[2];
+      let productname = infos[3];
+      let uoM = infos[4];
+
+      let insertingRow = [storename,storecode,barcode,productname,uoM,...value]
+      if (insertingRow.length < 52){
+        let zerosArray = []
+        for (let i = 0; i < 52 - insertingRow.length; i++){
+          zerosArray.push(0);
+        }
+        insertingRow = [...insertingRow, ...zerosArray];
+      }
+
+      resultArray.push(insertingRow);
+    })
+
+    return resultArray
 }
